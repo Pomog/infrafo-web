709760e5950ed16851cadcc0637d9b509d3bc102
Yurii Panasuik <thoryur@gmail.com>
2025-08-31 22:14:04 +0300

    Adding time travel

--- infrafo-web/src/testing/board.tsx
+++ infrafo-web/src/testing/board.tsx
@@ -7,9 +7,9 @@ export default function Board() {
     type Cell = 'X' | 'O' | null;
 
     const LINES = [
-        [0,1,2], [3,4,5], [6,7,8], // row
-        [0,3,6], [1,4,7], [2,5,8], // columns
-        [0,4,8], [2,4,6],   // diagonal
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], // row
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], // columns
+        [0, 4, 8], [2, 4, 6],   // diagonal
     ] as const satisfies ReadonlyArray<readonly [number, number, number]>;
 
     const emptyBoard = useCallback(() => Array(9).fill(null), []);
@@ -54,20 +54,21 @@ export default function Board() {
         <>
             <div className="status">{gameStatus}</div>
             <div className="board-row">
-                <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-                <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-                <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+                <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
+                <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
+                <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
             </div>
             <div className="board-row">
-                <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-                <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-                <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+                <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
+                <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
+                <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
             </div>
             <div className="board-row">
-                <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-                <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-                <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+                <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
+                <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
+                <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
             </div>
+            <button onClick={reset}>Reset</button>
         </>
     )
 }
