fe3f825f146b1953bdf7b28957d6be866af07551
Yurii Panasuik <thoryur@gmail.com>
2025-11-21 23:26:44 +0200

    detailed plan for CoachV4

--- infrafo-web/src/app/swim/Pool/swimmer/CoachV4.ts
+++ infrafo-web/src/app/swim/Pool/swimmer/CoachV4.ts
@@ -1,6 +1,6 @@
 import {ActorV2} from "@/app/swim/Pool/swimmer/ActorV2";
 import {Actor} from "@/app/swim/Pool/Actor";
-import {OK, StepResult, UnitVector} from "@/app/swim/Pool/Types";
+import {CAUGHT, OK, StepResult, UnitVector} from "@/app/swim/Pool/Types";
 
 /**
  * Convention:
@@ -16,16 +16,37 @@ export class CoachV4 extends ActorV2 {
         poolRadius: number,
         poolCenter: {x:number;y:number},
         position: {x:number;y:number},
-        private readonly dir: 1 | -1 = 1,
     ) {
         super(speed, poolRadius, poolCenter, position);
     }
 
+    /** Rotate the radial vector by +90° */
     private tangentCCW(): UnitVector {
         const vr = this.radialUnitFromCenter();
         return { ux: -vr.uy, uy: vr.ux };
     }
+
+    public getSignedAngularVelocity(): number {
+        const r = this.vecFrom(this.getPoolCenter()).len;
+        return (r > 0) ? this.dir * (this.speed / r) : 0;
+    }
     update(opponent: Actor, dt: number): StepResult {
+        // BEFORE moving сheck isCaught
+
+        // decide direction along the rim: +1 = CCW, -1 = CW, 0 = stay
+
+        // Angular step: ΔA = (v*dt)/R, signed by dir
+
+        // Current radial vector (center -> coach)
+
+        // Scale radial vector to exact radius (remove drift)
+
+        // Rotate by ΔA (strictly along arc)
+
+        // New position on the circle
+
+        // AFTER moving
+        if (this.isCaught(opponent)) return CAUGHT;
         return OK;
     }
 }
